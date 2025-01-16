aa9d1dd4a90bfb30d41c8494592d45446396e168
ArthurRufi <temparthur87@gmail.com>
2025-01-16 08:29:00 -0300

    <modelagem> modelando logica de negocios para o receitas e despesas

--- gestaofinanceira/receitasdespesas/models.py
+++ gestaofinanceira/receitasdespesas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class ReceitasDespesas(models.Model):
+    #colocar na documentação 1 = Receita, 2 = Despesas
+    tipo = models.IntegerField(default=2)
+    valor = models.FloatField(default=0.0)
+    dataDaMovimentacao = models.DateTimeField()
+    descrição = models.CharField()
+
+
