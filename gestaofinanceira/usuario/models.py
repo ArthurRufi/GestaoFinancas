2355dc2a2559c552c0080960684299752dbc0dce
ArthurRufi <temparthur87@gmail.com>
2025-01-16 08:05:46 -0300

    <modelagem> modelando logica de negocios para o Usuario

--- gestaofinanceira/usuario/models.py
+++ gestaofinanceira/usuario/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    userEmail = models.EmailField()
+    dataDeNascimento =models.DateField()
+    senhaUser = models.CharField()
+
+    class Meta:
+        ordering = ['name']
+
+        
