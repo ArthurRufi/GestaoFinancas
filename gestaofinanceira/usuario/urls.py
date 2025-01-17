f2e67c8bad8bc593e5fac0404a2af542056856f2
ArthurRufi <temparthur87@gmail.com>
2025-01-17 17:21:06 -0300

    correção em bug de cadastro de usuario

--- gestaofinanceira/usuario/urls.py
+++ gestaofinanceira/usuario/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CadastrarUsuario
+from .views import CadastrarUsuario, EncontrarUsuario
 
 
 urlpatterns = [
-    path('criar/', CadastrarUsuario.as_view(), name='criarUsuario')
+    path('criar/', CadastrarUsuario.as_view(), name='criarUsuario'),
+    path('consultar/<str:usuario>/', EncontrarUsuario.as_view(), name='consultarUsuario'),
 ]
--- gestaofinanceira/usuario/views.py
+++ gestaofinanceira/usuario/views.py
@@ -5,14 +5,42 @@ from rest_framework import status
 from .models import User
 from .serializers import UserSerializer
 
-
 class CadastrarUsuario(APIView):
-    def post(self, request, *args, **kwargs):
-        serializers = UserSerializer(data=request.data)
+    def post(self, request):
+        data = request.data
+        try:
+            user = User.objects.create(
+                name=data.get('name'),
+                userEmail=data.get('userEmail'),
+                dataDeNascimento=data.get('dataDeNascimento'),
+                senhaUser=data.get('senhaUser')
+            )
+            user.save()
+            return Response({"message": "Usuário criado com sucesso!"}, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+    
 
-        if serializers.is_valid():
-            serializers.save
-            return Response(serializers.data, status=status.HTTP_201_CREATED)
-    
-        return Response(serializers.errors, status=status.HTTP_201_CREATED)
-    
+class EncontrarUsuario(APIView):
+    def get(self, request, usuario):
+        # O parâmetro da URL já é o nome do usuário
+        username = usuario
+
+        # Tenta buscar o usuário pelo nome
+        usuarioEntregue = User.objects.filter(name=username)
+
+        if not usuarioEntregue.exists():
+            return Response(
+                {"Alert Message": "Usuário não encontrado"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        # Caso encontre, retorna as informações
+        return Response(
+            {
+                "name": usuarioEntregue.first().name,
+                "email": usuarioEntregue.first().userEmail,
+                "dataDeNascimento": usuarioEntregue.first().dataDeNascimento,
+            },
+            status=status.HTTP_200_OK,
+        )
