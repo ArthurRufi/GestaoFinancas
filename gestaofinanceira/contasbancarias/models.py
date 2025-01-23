fbe3ce7bfbe039e690cefd14104dd28e4a5fdaa4
ArthurRufi <temparthur87@gmail.com>
2025-01-23 16:34:19 -0300

    feat novas funcionalidades adicinadas ao receitasdespesas

--- gestaofinanceira/contasbancarias/models.py
+++ gestaofinanceira/contasbancarias/models.py
@@ -5,4 +5,9 @@ class ContasUsuario(models.Model):
     emailUsuario = models.CharField(max_length=300)
     bancoUsuario = models.CharField(max_length=255)
     agenciaUsuario = models.IntegerField()
-    contaUsuario =models.IntegerField()
+    contaUsuario = models.IntegerField()
+    '''
+    
+    #essa informações servirão para consulta simbolica
+    #Em um futuro deve-se adicionar outro serviço somente para confirmação de CPF e etc, separando esse serviço do principal
+    '''
--- gestaofinanceira/receitasdespesas/serializers.py
+++ gestaofinanceira/receitasdespesas/serializers.py
@@ -1,24 +1,21 @@
 from rest_framework import serializers
 from receitasdespesas.models import ReceitasDespesas
 
-
-class DespesaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ReceitasDespesas
-        fields = ['id', 'tipo', 'valor', 'dataDaMovimentacao', 'descricao']
     
-    def validate_tipo(self, value):
-        if value != 2:
-            raise serializers.ValidationError("O tipo deve ser 2 para despesas.")
-        return value
-    
-
-class ReceitaSerializer(serializers.ModelSerializer):
+class BaseRDSerializer(serializers.ModelSerializer):
     class Meta:
         model = ReceitasDespesas
         fields = ['id', 'tipo', 'valor', 'dataDaMovimentacao', 'descricao', 'usuario_email']
     
+
+class ReceitaSerializer(BaseRDSerializer):
     def validate_tipo(self, value):
         if value != 1:
             raise serializers.ValidationError("O tipo deve ser 1 para receitas.")
         return value
+        
+
+class DespesasSerializer(BaseRDSerializer):
+    def validate_tipo(self, value):
+        if value !=2:
+            raise serializers.ValidationError("O tipo deve ser 2 para despesas")
