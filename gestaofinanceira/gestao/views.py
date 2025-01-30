05500aa059ae04aef5209cd3961af1e6155850b5
ArthurRufi <temparthur87@gmail.com>
2025-01-29 22:51:38 -0300

    sei la porra, adicionei coisa pra krl

--- gestaofinanceira/gestao/views.py
+++ gestaofinanceira/gestao/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
 # Create your views here.
+
+
+class graficalInformationsAPI(APIView):
+    
+    #arquivo gerador para mini IA para explicar como anda os gastos pessoais
+    pass
+
+
+class situacaoFinanceira(APIView):
+    #deve-se avisar o usuaio sobre sua situação fianceira atual
+    pass
