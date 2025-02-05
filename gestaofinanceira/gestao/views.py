e012e3f378d16261cd9fa5815a09ea73915f70f4
ArthurRufi <temparthur87@gmail.com>
2025-02-04 23:07:46 -0300

    parte de logica para gestao de financças para micro avisos modelada

--- gestaofinanceira/gestao/views.py
+++ gestaofinanceira/gestao/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from gestaofinanceira.gestao.utils.gerador_mensagens import EmailExcessiveSpending, EmailPasswordRecover
+from gestaofinanceira.receitasdespesas.models import ReceitasDespesas
+
 # Create your views here.
 
 
@@ -13,4 +15,15 @@ class ArtificialInteligenceForVicesAPI(APIView):
 class situacaoFinanceira(APIView):
     #deve-se avisar o usuaio sobre sua situação fianceira atual
     #pegar gastos mensais e indicar prejuizo ou lucro
+    def mesNegativado():
+        #pegar no banco de dados as informações da conta e lançar se estar negativado
+        pass
+    def granaSobrando():
+        #avisa se está com grana sobrando OBS: AVISAR SE SOBROU MAIS DE 20% DOS GANHOS MENSAIS
+        pass
+    def juntandoGrana():
+        #avisar que falta depositar os valores para a meta
+        pass
+    
+
     pass
